0d3bab276bdb8cdc8b86923d15dd6a3128bd6fa1
wibawasuyadnya <dekgusnfs@gmail.com>
2022-12-08 00:25:31 +0800

    add more functionality

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-note-details',
   templateUrl: './note-details.component.html',
   styleUrls: ['./note-details.component.css']
 })
-export class NoteDetailsComponent {
+export class NoteDetailsComponent implements OnInit {
 
+    note: Note;
+
+    constructor( private notesServices: NotesService, private router: Router ){  }
+
+    ngOnInit() {
+      this.note = new Note();
+    }
+
+
+    onSubmit(form: NgForm) {
+      this.notesServices.add(form.value);
+      this.router.navigateByUrl('/');
+    } 
+
+    cancel(){
+      this.router.navigateByUrl('/'); 
+    }
 }
--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
   styleUrls: ['./notes-list.component.css']
 })
-export class NotesListComponent implements OnInit{
-  cardTitle: string = 'Kadek Ngentot';
-  constructor(){
+export class NotesListComponent implements OnInit {
 
-  }
+  notes: Note[] = new Array<Note>();
+
+  constructor(private notesService: NotesService){ }
 
   ngOnInit() {
-    
+    this.notes = this.notesService.getAll();
   }
 }
