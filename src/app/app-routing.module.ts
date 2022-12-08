65d84f6d0a43950695b140b370a24611aa5283c4
wibawasuyadnya <dekgusnfs@gmail.com>
2022-12-08 19:39:50 +0800

    set route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     { path: '', component: NotesListComponent },
     { path: 'new', component: NoteDetailsComponent},
     { path: ':id', component: NoteDetailsComponent}
-  ]}
+  ]} 
 ];
 
 @NgModule({
