0f1920df94df2d03f4d597db594ea9bb0eee74ae
wibawasuyadnya <dekgusnfs@gmail.com>
2022-12-08 19:39:44 +0800

    set index route

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
 
@@ -12,15 +12,31 @@ import { NotesService } from 'src/app/shared/notes.service';
 export class NoteDetailsComponent implements OnInit {
 
     note: Note;
+    noteId: number;
+    new: boolean;
 
-    constructor( private notesServices: NotesService, private router: Router ){  }
+    constructor( private notesServices: NotesService, private router: Router, private route : ActivatedRoute ){  }
 
     ngOnInit() {
+
+      this.route.params.subscribe((params: Params) => {
+        if(params['id']) {
+          this.note = this.notesServices.get(params['id']);
+          this.noteId = params['id'];
+          this.new = false;
+        } else {
+          this.new = true;
+        }
+      })
+
       this.note = new Note();
     }
 
 
     onSubmit(form: NgForm) {
+
+      
+
       this.notesServices.add(form.value);
       this.router.navigateByUrl('/');
     } 
