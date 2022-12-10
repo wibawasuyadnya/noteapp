16edd362e7e76dd127c541526172cb7830e72fa0
wibawasuyadnya <dekgusnfs@gmail.com>
2022-12-10 18:51:18 +0800

    add filter search

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,4 +1,4 @@
-import { transition, trigger, style, animate } from '@angular/animations';
+import { transition, trigger, style, animate, query, stagger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
@@ -18,19 +18,13 @@ import { NotesService } from 'src/app/shared/notes.service';
             'margin-bottom': 0,
 
             //expand padding for certain browser bug
-            padingTop: 0,
-            paddingBottom: 0,
-            PaddingRight: 0,
-            PaddingLeft: 0
+            padding: 0,
           }),
           //animation spacing margin and height
            animate('50ms', style({
             height: '*',
-            'margin-b0ttom': '*',
-            padingTop: '*',
-            paddingBottom: '*',
-            PaddingRight: '*',
-            PaddingLeft: '*'
+            'margin-bottom': '*',
+            padding: '*',
            })),
            animate(68)
         ]),
@@ -40,11 +34,41 @@ import { NotesService } from 'src/app/shared/notes.service';
             animate(50, style({
               transform: 'scale(1.05)',
             })),
-            //second is scale down 
-            
+            //second is scale down back to normal and beginning to fade out
+            animate(50, style({
+              transform: 'scale(1)',
+              opacity: 0.75,
+            })),
+            //scale down and fade out
+            animate('120ms ease-out', style({
+              transform: 'scale(0.68)',
+              opacity: 0,
+            })),
+            //animate spacing height, margin and padding
+            animate('150ms ease-out', style({
+              height: 0,
+              padding: 0,
+              'margin-left': 0,
+            }))
         ])
+    ]),
 
+    trigger('listAnim', [
+      transition('* => *', [
+         query(':enter', [ 
+            style({
+              opacity: 0, 
+              height: 0,
+            }),
+            stagger(100, [
+              animate('0.2s ease')
+            ])
+         ], {
+            optional: true,
+         })
+      ])
     ])
+
   ]
 
 })
@@ -60,6 +84,39 @@ export class NotesListComponent implements OnInit {
 
   deleteNote(id: number){
     this.notesService.delete(id);
+  }
+
+  filter(query: string){
+    query = query.toLowerCase().trim();
+
+    let allResults :  Note[] = new Array<Note>();
+    
+    let terms : string[] =  query.split(' ');
+    terms = this.removeDuplicates(terms);
+    terms.forEach(term => {
+      let results: Note[] = this.relevantNotes(terms);
+      //append all results to allResults array
+    })
+  }
+
+  removeDuplicates(arr: Array<any>) : Array<any> {
+    let uniqueResults: Set<any> = new Set<any>;
+    arr.forEach(e => uniqueResults.add(e)); 
+
+    return Array.from(uniqueResults);
+
+  }
+
+  relevantNotes(query: any) {
+    query = query.toLowerCase().trim();
+    let relevantNotes = this.notes.filter(note => {
+      if(note.body.toLowerCase().includes(query) || note.title.toLowerCase().includes(query)){
+        return true;
+      } else {
+        return false;
+      }
+    })
+    return relevantNotes;
   }
 
 }
